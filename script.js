5393071bc7290e95a83aa4f37e525dd7e6addc57
kenung0987 <54462061+kenung0987@users.noreply.github.com>
2023-11-18 00:44:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function showMessage() {
-    const message = "Selamat atas keberhasilan Anda dalam menyelesaikan studi! Semoga masa depan penuh dengan kesuksesan dan kebahagiaan.";
+    const message = "Selamat atas keberhasilanmu, Faza! Semoga setiap langkahmu penuh dengan prestasi dan kebahagiaan.";
     document.getElementById("wisudaMessage").innerText = message;
 }
 
-// Tambahkan script untuk mengontrol audio
 const audio = document.getElementById("audio");
 
 window.addEventListener("scroll", function() {
     const value = window.scrollY;
-    audio.volume = value / 2000; // Sesuaikan dengan tingkat volume yang diinginkan
+    audio.volume = value / 2000;
 });
 
 audio.play();
-￼Enter
